ba3bd136bd480512f945e2c21de908d4be9e7f8b
Meddy Brai <meddy.brai@nurun.com>
2024-08-06 11:36:50 -0400

    Add wezterm settings

---
+++ dot_wezterm.lua
@@ -0,0 +1,72 @@
+-- Pull in the wezterm API
+local wezterm = require("wezterm")
+
+-- This will hold the configuration.
+local config = wezterm.config_builder()
+
+-- This is where you actually apply your config choices
+
+local function get_appearance()
+	if wezterm.gui then
+		return wezterm.gui.get_appearance()
+	end
+	return "Dark"
+end
+
+local function scheme_for_appearance(appearance)
+	if appearance:find("Dark") then
+		return "Dracula (Official)"
+	else
+		return "Londontube (light) (terminal.sexy)"
+	end
+end
+
+-- Apply the color scheme
+config.color_scheme = scheme_for_appearance(get_appearance())
+
+-- Other configurations
+config.font = wezterm.font("MesloLGS NF")
+config.window_decorations = "RESIZE"
+config.check_for_updates = false
+config.window_background_opacity = 0.95
+config.macos_window_background_blur = 20
+
+-- Key bindings
+config.keys = {
+	-- Split horizontal
+	{
+		key = "d",
+		mods = "CMD",
+		action = wezterm.action.SplitHorizontal({ domain = "CurrentPaneDomain" }),
+	},
+	-- Split vertical
+	{
+		key = "d",
+		mods = "CMD|SHIFT",
+		action = wezterm.action.SplitVertical({ domain = "CurrentPaneDomain" }),
+	},
+	-- Navigation entre les panes
+	{
+		key = "LeftArrow",
+		mods = "CMD",
+		action = wezterm.action.ActivatePaneDirection("Left"),
+	},
+	{
+		key = "RightArrow",
+		mods = "CMD",
+		action = wezterm.action.ActivatePaneDirection("Right"),
+	},
+	{
+		key = "UpArrow",
+		mods = "CMD",
+		action = wezterm.action.ActivatePaneDirection("Up"),
+	},
+	{
+		key = "DownArrow",
+		mods = "CMD",
+		action = wezterm.action.ActivatePaneDirection("Down"),
+	},
+}
+
+-- and finally, return the configuration to wezterm
+return config
